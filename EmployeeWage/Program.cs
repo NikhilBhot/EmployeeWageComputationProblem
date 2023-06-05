34775ecf446be7dda8b0b31ab84ebcde1007806d
unknown <bhotnikhil25@gmail.com>
2023-06-05 17:41:39 +0530

    [Nikhil]-Wel-Come To Employee Wage Computation Problem

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-come To Employee Wage Computation Problem");
+        }
+    }
+}
